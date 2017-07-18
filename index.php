46e332e99051190c9cf728065418c45399ccdd52
Bri Dickman <brianna.dickman@gmail.com>
2017-07-18 18:48:14 -0500

    working with arrays and array keys. installed php to work on running local php server

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   $show1 = 'Outlander';
   $show2 = 'Game of Thrones';
   $fruitArray = ['apples', 'watermelon', 'pineapple', 'blueberries', 'oranges'];
+  $vegetableArray = ['green peppers', 'carrots', 'tomato', 'brussel sprouts', 'onions'];
 ?>
 
 <!DOCTYPE html>
@@ -30,6 +31,30 @@
       $last_fruit = $fruitArray($last_index);
       echo 'My favorite fruit is: $first_fruit';
       echo 'My least favorite fruit is: $last_fruit';
+
+      $all_produce = array_merge($fruitArray, $vegetableArray);
+      echo 'Here is all of the produce I have: $all_produce';
+
+      $phone_numbers = [
+        'Ben' => '651-769-5235',
+        'Mom' => '218-850-0519',
+        'Me' => '701-429-4214',
+        'Kelsie' => '218-849-7962'
+      ];
+
+      echo 'The phone numbers I have memorized are: $phone_numbers';
+
+      if (array_key_exists('Kelsie', $phone_numbers)) {
+        echo 'Kelsies phone number is: $phone_numbers("Kelsie")';
+      } else {
+        echo 'Kelsies phone number is not a number you have memorized!';
+      }
+
+      if (array_key_exists('Rachel', $phone_numbers)) {
+        echo 'Rachels phone number is: $phone_numbers("Rachel")';
+      } else {
+        echo 'Rachels phone number is not a number you have memorized!';
+      }
     ?>
   </p>
 
