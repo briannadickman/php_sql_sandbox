0e681bec09b82a7ba0f061496b95549aabe4803c
Bri Dickman <brianna.dickman@gmail.com>
2017-07-17 20:22:49 -0500

    working with PHP arrays and PHP reset, count, and grabbing last index in an array

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
   $greeting = 'Hello World';
   $show1 = 'Outlander';
   $show2 = 'Game of Thrones';
+  $fruitArray = ['apples', 'watermelon', 'pineapple', 'blueberries', 'oranges'];
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +23,15 @@
       <li><?php echo $show1; ?></li>
       <li><?php echo $show2; ?></li>
     </ul>
+    <h3>My Favorite Fruits</h3>
+    <p><?php echo $fruitArray
+      $first_fruit = reset($fruitArray);
+      $last_index = count($fruitArray) - 1;
+      $last_fruit = $fruitArray($last_index);
+      echo 'My favorite fruit is: $first_fruit';
+      echo 'My least favorite fruit is: $last_fruit';
+    ?>
+  </p>
 
   </body>
 </html>
