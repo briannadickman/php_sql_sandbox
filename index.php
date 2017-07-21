f826ba707e601064ea5e2d21e33cbcf5c9832a69
Bri Dickman <brianna.dickman@gmail.com>
2017-07-21 15:54:07 -0500

    while loops and functions in PHP

--- index.php
+++ index.php
@@ -73,6 +73,30 @@
       for($i = 0; $i < count($vegetableArray); $i++){
         echo $vegetableArray[$i] . "\n";
       }
+
+      $counter = 0;
+
+      while ($counter < 10){
+        $counter += 1;
+        if ($counter % 2 == 0){
+          echo 'Skipping even number';
+          continue;
+        }
+        echo 'Count is now at: $counter.\n';
+      }
+
+      // Write the function squared_sum here
+      function squared_sum($numbers){
+          $total_square_sum = 0;
+          foreach ($numbers as $number) {
+          	$square_sum = $number * $number;
+              $total_square_sum += $square_sum;
+          }
+          echo $total_square_sum;
+      }
+
+      echo squared_sum([56, 65, 26, 86, 66, 34, 78, 74, 67, 18, 34, 73, 45, 67, 75, 10, 60, 80, 74, 16, 86, 34, 12, 23, 42, 72, 36, 3, 73, 9, 92, 81, 94, 54, 97, 74, 45, 55, 70, 94, 96, 81, 86, 86, 84, 4, 32, 8, 96, 86, 87, 18, 84, 87, 59, 48, 32, 90, 17, 22, 82, 79, 66, 28, 17, 14, 80, 83, 66, 36, 21, 89, 68, 2, 51, 65, 20, 87, 48, 5, 1, 16, 60, 53, 84, 90, 16, 2, 37, 73, 57, 70, 57, 69, 68, 1, 24, 40, 72, 97]);
+
     ?>
   </p>
 
